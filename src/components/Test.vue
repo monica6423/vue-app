373f7794881e4a7a1b397fce1f1f1419f54588c4
monica6423 <monica6423@gmail.com>
2020-08-02 19:32:12 +0800

    init commit

---
+++ src/components/Test.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="test">
+        我是test key
+        {{msg}}
+    </div>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            msg:"i am from msg"
+        }
+    }
+}
+</script>
+
+<style scoped>
+.test {
+    color: red;
+}
+</style>
